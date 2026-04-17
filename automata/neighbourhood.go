037e8b6d58b73938a8a919bd403565cfc0c00aef
Rob Waller <rdwaller1984@gmail.com>
2026-04-17 12:11:49 +0100

    Update neighbourhood function to correctly implement test implementation.

--- automata/neighbourhood.go
+++ automata/neighbourhood.go
@@ -3,8 +3,16 @@ package automata
 import "go_automata/rules"
 
 func FindNeighbourhood(rule uint8, neighbourhood [3]uint8) rules.RuleEngine {
-	return rules.RuleEngine{
-		Neighbourhood: [3]uint8{1, 1, 1},
-		NextStep:      1,
+	ruleEngines := rules.MakeRuleEngine(rule)
+
+	var ruleEngine rules.RuleEngine
+
+	for _, engine := range ruleEngines {
+		if engine.Neighbourhood == neighbourhood {
+			ruleEngine = engine
+			break
+		}
 	}
+
+	return ruleEngine
 }
