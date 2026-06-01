94c9b1032a182f5f35746e8489887ce177929bf1
Rob Waller <rdwaller1984@gmail.com>
2026-06-01 14:02:26 +0100

    Created first version of get neighbourhood functionality.

--- automata/neighbourhood.go
+++ automata/neighbourhood.go
@@ -2,6 +2,15 @@ package automata
 
 import "go_automata/rules"
 
+func GetNeighbourhood(row []uint8, position uint8) [3]uint8 {
+	n := len(row)
+	i := int(position)
+	prev := row[(i+n-1)%n]
+	curr := row[i]
+	next := row[(i+1)%n]
+	return [3]uint8{prev, curr, next}
+}
+
 func FindNeighbourhood(rule uint8, neighbourhood [3]uint8) (rules.RuleEngine, bool) {
 
 	for _, engine := range rules.MakeRuleEngine(rule) {
--- automata/neighbourhood_test.go
+++ automata/neighbourhood_test.go
@@ -8,6 +8,61 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGetNeighbourhood(t *testing.T) {
+	t.Run("get neighbourhood", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			row      []uint8
+			position uint8
+			expected [3]uint8
+		}{
+			{
+				name:     "middle position",
+				row:      []uint8{0, 1, 0, 1, 1},
+				position: 2,
+				expected: [3]uint8{1, 0, 1},
+			},
+			{
+				name:     "start of row wraps previous to end",
+				row:      []uint8{0, 1, 0, 1, 1},
+				position: 0,
+				expected: [3]uint8{1, 0, 1},
+			},
+			{
+				name:     "end of row wraps next to start",
+				row:      []uint8{0, 1, 0, 1, 1},
+				position: 4,
+				expected: [3]uint8{1, 1, 0},
+			},
+			{
+				name:     "single element row wraps both prev and next to itself",
+				row:      []uint8{1},
+				position: 0,
+				expected: [3]uint8{1, 1, 1},
+			},
+			{
+				name:     "two element row position 0",
+				row:      []uint8{0, 1},
+				position: 0,
+				expected: [3]uint8{1, 0, 1},
+			},
+			{
+				name:     "two element row position 1",
+				row:      []uint8{0, 1},
+				position: 1,
+				expected: [3]uint8{0, 1, 0},
+			},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				result := GetNeighbourhood(test.row, test.position)
+				assert.Equal(t, test.expected, result)
+			})
+		}
+	})
+}
+
 func TestFindNeighbourhood(t *testing.T) {
 
 	type testCase struct {
