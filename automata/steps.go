708818ff98c78cf344e751100050e0946aebcddb
Rob Waller <rdwaller1984@gmail.com>
2026-06-09 14:19:41 +0100

    Merged the functionality in the step.go file into the steps.go file, also merged the test files.

--- automata/steps.go
+++ automata/steps.go
@@ -1,5 +1,17 @@
 package automata
 
+func Step(engines []RuleEngine, row []uint8) []uint8 {
+	result := make([]uint8, len(row))
+
+	for i := range row {
+		n := GetNeighbourhood(row, uint8(i))
+		engine, _ := FindNeighbourhood(engines, n)
+		result[i] = engine.NextStep
+	}
+
+	return result
+}
+
 func Steps(steps uint, rule uint8, seed []uint8) map[uint][]uint8 {
 	engines := MakeRuleEngine(rule)
 	result := make(map[uint][]uint8)
